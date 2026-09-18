19d62bc03a11afda07e821629d38d0ee3402f1d8
Naczelny09 <jakubkorkowski@gmail.com>
2026-09-18 08:44:38 +0200

    Dodane 6 testow postaci

--- CharacterSelectTest/Tests/Domain/Enum/SummonerTest.cs
+++ CharacterSelectTest/Tests/Domain/Enum/SummonerTest.cs
@@ -1,12 +1,51 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CharacterSelectTest.Domain.Character;
 
-namespace CharacterSelectTest.Tests.Domain.Enum
+namespace CharacterSelectTest.Tests
 {
-    internal class SummonerTest
+    [TestClass]
+    public class SummonerTests
     {
+        [TestMethod]
+        public void Creation_HasCorrectName()
+        {
+            var summoner = new Summoner("Yorick");
+
+            Assert.AreEqual("Yorick", summoner.Name);
+        }
+
+        [TestMethod]
+        public void Creation_HasCorrectBaseStats()
+        {
+            var summoner = new Summoner("Yorick");
+
+            Assert.AreEqual(30, summoner.Health);
+            Assert.AreEqual(2, summoner.Strength);
+            Assert.AreEqual(15, summoner.Intelligence);
+            Assert.AreEqual(8, summoner.Agility);
+            Assert.AreEqual(15, summoner.Crit_rate);
+            Assert.AreEqual(100, summoner.Crit_DMG);
+        }
+
+        [TestMethod]
+        public void Attak_ReturnsCorrectValue()
+        {
+            var summoner = new Summoner("Yorick");
+
+            double actualAttack = summoner.Attak();
+
+            Assert.AreEqual(20.4, actualAttack);
+        }
+
+        [TestMethod]
+        public void SpecialAbility_ReturnsCorrectMessage()
+        {
+            var summoner = new Summoner("Yorick");
+            string expectedMessage = "Summoner summons 5 bats that attacks enemies";
+
+            string actualMessage = summoner.SpecialAbility();
+
+            Assert.AreEqual(expectedMessage, actualMessage);
+        }
     }
-}
+}s
